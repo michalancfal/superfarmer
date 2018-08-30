ffcf0c8ddae7a6bd9524d74702beea591cb3dbf1
miniozaur <ancfal.michal@gmail.com>
2018-08-30 18:11:21 +0200

    Dla Tomka :)

--- src/main/java/pl/superfarmer/game/superfarmer/domain/Farm.java
+++ src/main/java/pl/superfarmer/game/superfarmer/domain/Farm.java
@@ -110,60 +110,181 @@ public class Farm implements Playable {
         return bigDogs;
     }
 
+    public int getTempWolves() {
+        return tempWolves;
+    }
+
+    public int getTempFoxes() {
+        return tempFoxes;
+    }
+
+    public void setTempWolves(int tempWolves) {
+        this.tempWolves = tempWolves;
+    }
+
+    public void setTempFoxes(int tempFoxes) {
+        this.tempFoxes = tempFoxes;
+    }
+
     int tempRabbits = 0;
     int tempSheep = 0;
     int tempPigs = 0;
     int tempCows = 0;
     int tempHorses = 0;
+    int tempWolves = 0;
+    int tempFoxes = 0;
+
+
+    public void resetTemp() {
+
+        this.setTempRabbits(0);
+        this.setTempSheep(0);
+        this.setTempPigs(0);
+        this.setTempCows(0);
+        this.setTempHorses(0);
+
+    }
 
     @Override
     public void countDiceResultsOnFarm(DiceThrowResult diceThrowResult) {
 
 
-        //Liczenie wyników dla użytkownika z wyniku kostki czerwonej : lis, koń, 2 świnie, 2 owce, 6 królików
+        //Liczenie przyrostów dla użytkownika z wyniku kostki czerwonej : lis, koń, świnie, owce, królików
 
 
         switch (diceThrowResult.getRedField()) {
 
             case ("rabbit"): {
-
-                setTempRabbits(1 + this.getRabbits());
-
+                setTempRabbits(getTempRabbits() + 1);
                 break;
-
             }
             case ("sheep"): {
-
-                setTempSheep(1 + this.getSheep());
-
+                setTempSheep(getTempSheep() + 1);
                 break;
-
             }
             case ("pig"): {
-
-                setTempPigs(1 + this.getPigs());
-
+                setTempPigs(getTempPigs() + 1);
                 break;
-
             }
             case ("horse"): {
-
-                setTempHorses(1 + this.getHorses());
-
+                setTempHorses(getTempHorses() + 1);
                 break;
-
-            }
-            case ("fox"): {
-
-
-                break;
-
             }
             default: {
-                System.out.println("Wynik kostki dał nieprawidłowy rezultat");
+                System.out.println("nieprawidłowy rezultat");
+
 
             }
         }
 
+        //Liczenie przyrostu dla użytkownika z wyniku rzutu kostki zieonej : wilk, krowa, świnia, owca, królik
+
+        switch (diceThrowResult.getGreenField()) {
+
+            case ("rabbit"): {
+                setTempRabbits(getTempRabbits() + 1);
+                break;
+            }
+
+            case ("sheep"): {
+                setTempSheep(getTempSheep() + 1);
+                break;
+            }
+
+            case ("pig"): {
+                setTempPigs(getTempPigs() + 1);
+                break;
+            }
+            case ("krowa"): {
+                setTempCows(getTempCows() + 1);
+                break;
+            }
+            case ("wilk"): {
+
+
+                break;
+            }
+            default: {
+                System.out.println("nieprawidłowy rezultat");
+
+            }
+
+        }
+
+
+        if (tempRabbits > 0) {
+
+            this.setRabbits((this.getRabbits() + tempRabbits) % 2);
+
+        }
+
+        if (tempSheep > 0) {
+            this.setSheep((this.getSheep() + tempSheep) % 2);
+
+        }
+        if (tempPigs > 0) {
+            this.setPigs((this.getPigs() + tempPigs) % 2);
+
+        }
+        if (tempCows > 0) {
+            this.setCows((this.getCows() + tempCows) % 2);
+
+        }
+        if (tempHorses > 0) {
+            this.setHorses((this.getHorses() + tempHorses) % 2);
+
+        }
+
+        if (tempFoxes > 0) {
+            if (this.getSmallDogs() > 0) {
+                this.setSmallDogs(this.getSmallDogs() - 1);
+
+                System.out.println("");
+            } else {
+                this.setRabbits(0);
+                System.out.println("zabrano wszystkie króliki");
+            }
+
+        }
+        if (tempWolves > 0) {
+            if (this.getBigDogs() > 0) {
+                this.setBigDogs(this.getBigDogs() - 1);
+            }
+
+        }
+
     }
+
+    @Override
+    public String toString() {
+        return "Farm{" +
+                "rabbits=" + rabbits +
+                ", sheep=" + sheep +
+                ", pigs=" + pigs +
+                ", cows=" + cows +
+                ", horses=" + horses +
+                ", smallDogs=" + smallDogs +
+                ", bigDogs=" + bigDogs +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Farm farma = new Farm();
+        farma.setRabbits(1);
+        farma.setSheep(1);
+        farma.setPigs(1);
+
+
+        System.out.println(farma);
+        farma.countDiceResultsOnFarm(farma.throwDices());
+        System.out.println(farma);
+
+
+
+    }
+
+
+
+
 }
